f3ec98d4250aae3f549a11acb7ab0f7f5abcd26d
Kamil Janeček <j@necek.com>
2019-11-23 13:39:11 +0100

    add euclidean squared

--- src/distance/distances.py
+++ src/distance/distances.py
@@ -3,19 +3,26 @@ import numpy as np
 
 
 def euclidean_distance(v1: pd.Series, v2: pd.Series) -> float:
-    if len(v1) != len(v2):
-        raise ValueError(f'Invalid input len(v1)={len(v1)} and len(v2)={len(v2)}')
+    _assert_equal_sizes(v1, v2)
 
-    res = v1 - v2
-    res = res ** 2
-    res = np.sqrt(res.sum())
-
+    res = np.sqrt(euclidean_squared_distance(v1, v2))
     return res
 
 
-def manhattan_distance(v1: pd.Series, v2: pd.Series) -> float:
-    pass
-
-
 def euclidean_squared_distance(v1: pd.Series, v2: pd.Series) -> float:
+    _assert_equal_sizes(v1, v2)
+
+    res = v1 - v2
+    res = res ** 2
+    return res.sum()
+
+
+def manhattan_distance(v1: pd.Series, v2: pd.Series) -> float:
+    _assert_equal_sizes(v1, v2)
+
     pass
+
+
+def _assert_equal_sizes(v1: pd.Series, v2: pd.Series):
+    if len(v1) != len(v2):
+        raise ValueError(f'Invalid input len(v1)={len(v1)} and len(v2)={len(v2)}')
--- test/distance/test_distances.py
+++ test/distance/test_distances.py
@@ -4,6 +4,8 @@ from numpy.testing import assert_almost_equal
 
 from src.distance import euclidean_distance
 
+from src.distance import euclidean_squared_distance
+
 
 @pytest.mark.parametrize('v1, v2, expected', [
     (
@@ -19,5 +21,23 @@ from src.distance import euclidean_distance
 ])
 def test_euclidean_distance(v1, v2, expected):
     res = euclidean_distance(v1, v2)
+
+    assert_almost_equal(res, expected, decimal=3)
+
+
+@pytest.mark.parametrize('v1, v2, expected', [
+    (
+            Series([45, 0.05]),
+            Series([60, 0.05]),
+            225
+    ),
+    (
+            Series([12.231, 13.231, 1524.1233, 1254.123, 12315.33]),
+            Series([1211.231, 12, 0, -24, -15.25]),
+            157437355.888
+    )
+])
+def test_euclidean_squared_distance(v1, v2, expected):
+    res = euclidean_squared_distance(v1, v2)
 
     assert_almost_equal(res, expected, decimal=3)
