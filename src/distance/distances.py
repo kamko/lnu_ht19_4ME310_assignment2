2a812ae5bca511fb973d7b6ba6208d6a7f71441d
Kamil Janeček <j@necek.com>
2019-11-23 13:28:37 +0100

    add euclidean distance 2

--- src/distance/distances.py
+++ src/distance/distances.py
@@ -3,6 +3,9 @@ import numpy as np
 
 
 def euclidean_distance(v1: pd.Series, v2: pd.Series) -> float:
+    if len(v1) != len(v2):
+        raise ValueError(f'Invalid input len(v1)={len(v1)} and len(v2)={len(v2)}')
+
     res = v1 - v2
     res = res ** 2
     res = np.sqrt(res.sum())
--- test/distance/test_distances.py
+++ test/distance/test_distances.py
@@ -12,12 +12,12 @@ from src.distance import euclidean_distance
             15
     ),
     (
-            Series([45, 0.05]),
-            Series([60, 0.05]),
-            15
+            Series([12.231, 13.231, 1524.1233, 1254.123, 12315.33]),
+            Series([1211.231, 12, 0, -24, -15.25]),
+            12547.404
     )
 ])
 def test_euclidean_distance(v1, v2, expected):
     res = euclidean_distance(v1, v2)
 
-    assert_almost_equal(res, expected)
+    assert_almost_equal(res, expected, decimal=3)
